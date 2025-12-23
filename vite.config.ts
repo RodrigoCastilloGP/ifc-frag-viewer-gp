fc154f1173932b0142c1c4cec20d5ae41dc2b9e5
Rodrigo Castillo Perez <rodrigo.castillo@corp.ggp.com.mx>
2025-12-23 14:50:21 -0600

    Deploy: GH Pages Actions + Vite base

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+﻿import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/ifc-frag-viewer-gp/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
+});
